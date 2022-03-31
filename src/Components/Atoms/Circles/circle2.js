84c0704a2d1d99b827d4cfcb2653ec5b73dc0245
Karol Szymański <karol2134@op.pl>
2022-03-31 14:35:45 +0200

    New Commit test

---
+++ src/Components/Atoms/Circles/circle2.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const circle = styled.div`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 4px;
+`;
+
+export default circle
