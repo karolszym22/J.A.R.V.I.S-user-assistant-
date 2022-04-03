9db33b0c5546b0a18b1fc3861e7421266a44a6cd
Karol Szymański <karol2134@op.pl>
2022-04-03 21:22:53 +0200

    New File:
    -GlobalStyles.js

--- src/Components/Views/Root/root.js
+++ src/Components/Views/Root/root.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import Circle from '../../Atoms/Circles/circle2';
+import GlobalStyle  from '../../../Theme/GlobalStyles';
+const Root = () =>
+(
+    <div>
+        <GlobalStyle/>
+        <h1>Hello Kdasdsasarol</h1>
+        <Circle>asdasdas</Circle>
+    </div>
+)
 
-
-const App = () => <h1>Hej</h1>
-
-export default App;
+export default Root;
---
+++ src/Theme/GlobalStyles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+src: url('Avengers/Stark-6p31.ttf');
+*, *::before, *::after
+{
+    box-sizing: border-box;
+}
+body {
+    font-family: 'Avengers';
+}
+ 
+`;
+
+export default GlobalStyle;
