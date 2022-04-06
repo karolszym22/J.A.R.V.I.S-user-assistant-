dec984209e5f74e2657d6aa7c40677bb8298f27c
Karol Szymański <karol2134@op.pl>
2022-04-06 18:16:08 +0200

    Exoport new components  to root.js
    -Menu
    -Time
    -Notes

--- src/Components/Views/Root/root.js
+++ src/Components/Views/Root/root.js
@@ -14,11 +14,33 @@ import WeatherCircle from '../../Atoms/Date/data_circle'
 import WeatherData from '../../Atoms/Date/data_data'
 import WeatherDay from '../../Atoms/Date/data_day'
 import WeatherNumber from '../../Atoms/Date/data_number'
+import MainMenu from '../../Organism/Menu/main-menu'
+import MenuContainer  from '../../Molecules/Menu/menu_container'
+import MenuNav from '../../Atoms/Menu/menu_nav'
+import MainTime from '../../Organism/Time/time-main'
+import TimeContainer  from '../../Molecules/Time/time_container'
+import TimeData from '../../Atoms/Time/time_data'
+import TimeCircle from '../../Atoms/Time/time_circle'
+
+import MainNotes from '../../Organism/Notes/notes_main'
+import NotesTitle from '../../Atoms/Notes/notes_title'
+import NotesData from '../../Atoms/Notes/notes_data'
+import NotesRectangleOne from '../../Atoms/Notes/notes_rectangle_1'
+import NotesRectangleTwo from '../../Atoms/Notes/notes_rectangle_2'
+import NotesRectangleThree from '../../Molecules/Notes/notes_container'
 const Root = () =>
 (
     <div>
         <GlobalStyle/>
-        <h1>Hello Kdasdsasarol</h1>
+         <MainMenu>
+            <MenuContainer>
+               <MenuNav>Głowna</MenuNav>
+               <MenuNav>Szukaj</MenuNav>
+               <MenuNav>Pogoda</MenuNav>
+               <MenuNav>Notatki</MenuNav>
+               <MenuNav>Pomoc</MenuNav>
+            </MenuContainer>
+         </MainMenu>
         <Circle>
             <CircleContainer>
                  <Circlethree/>
@@ -41,6 +63,28 @@ const Root = () =>
               </WeatherCircle>
            </WeatherContainer>
         </WeatherMain>
+        <MainTime >
+           <TimeContainer>
+              <TimeCircle>
+              <TimeData>
+                 23
+              </TimeData>
+      
+              </TimeCircle>
+              
+           </TimeContainer>
+        </MainTime>
+        <MainNotes>
+           <NotesTitle>Notes</NotesTitle>
+           <NotesRectangleOne></NotesRectangleOne>
+           <NotesRectangleTwo></NotesRectangleTwo>
+           <NotesRectangleThree>
+              <NotesData>Karol</NotesData>
+              <NotesData>Karol</NotesData>
+              <NotesData>Karol</NotesData>
+              <NotesData>Karol</NotesData>
+           </NotesRectangleThree>
+        </MainNotes>
     </div>
 )
 
