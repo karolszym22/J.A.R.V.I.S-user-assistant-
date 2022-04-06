dc915394f3f69d62fa5d86d07860e8cdc3588636
Karol Szymański <karol2134@op.pl>
2022-04-06 18:13:45 +0200

    Add new components
    - menu_nav.js
    - main-menu.js
    - menu_container.js

---
+++ src/Components/Atoms/Menu/menu_nav.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Menu_nav = styled.p`
+    color: #00cdd1;
+    font-size: 24px;
+    cursor: pointer;
+    &:hover {
+        color: #b2dbe6;;
+    }
+       
+  
+`;
+
+export default Menu_nav
---
+++ src/Components/Molecules/Menu/menu_container.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Menu_Container = styled.div`
+    width: 60%;
+    height: 50px;
+    display: flex ;
+    justify-content: space-around;
+    align-items: center
+    
+`;
+
+export default Menu_Container 
