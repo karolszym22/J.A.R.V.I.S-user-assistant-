9c138507135e27a8691d55f0213bf0be41d6253f
Karol Szymański <karol2134@op.pl>
2022-05-02 09:13:07 +0200

    add react-router-dom and main-menu.js

--- src/Components/Organism/Menu/main-menu.js
+++ src/Components/Organism/Menu/main-menu.js
@@ -1,9 +1,25 @@
 import styled from 'styled-components'
+import MenuContainer from '../../Molecules/Menu/menu_container';
+import MenuNav from '../../Atoms/Menu/menu_nav'
+import {NavLink} from 'react-router-dom';
 
-const Menu = styled.div`
+const StyledDiv = styled.div`
     width: 100%;
     display: flex;
     justify-content: center;
 `;
+
+const Menu = () =>
+(
+    <StyledDiv>
+        <MenuContainer>
+              <MenuNav as={NavLink} to="/">Glowna</MenuNav>
+              <MenuNav as={NavLink} to="/Weather">Pogoda</MenuNav>
+              <MenuNav as={NavLink} to="/Notes">Notatki</MenuNav>
+              <MenuNav as={NavLink} to="/Help">Pomoc</MenuNav>
+        </MenuContainer>
+    </StyledDiv>
+)
+
 
 export default Menu
