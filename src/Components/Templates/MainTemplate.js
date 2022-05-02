2d87572b6877cf9cc144684eeac7614ee703c9e4
Karol Szymański <karol2134@op.pl>
2022-05-02 09:10:53 +0200

    add MainTemplate.js

---
+++ src/Components/Templates/MainTemplate.js
@@ -0,0 +1,25 @@
+import React from "react"
+import PropTypes from 'prop-types'
+import { ThemeProvider } from "styled-components"
+import GlobalStyle from "../../Theme/GlobalStyles"
+import { theme } from "../../Theme/main_theme"
+import Main from '../Organism/Menu/main-menu'
+
+
+const MainTemplate = ({children}) =>
+(
+    <div>
+      <GlobalStyle/>
+      <Main></Main>
+      <ThemeProvider theme={theme}>
+        <>{children}</>
+        
+     </ThemeProvider>
+    </div>
+)
+
+MainTemplate.propTypes = {
+    children: PropTypes.element.isRequired
+}
+
+export default MainTemplate;
