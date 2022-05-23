3201971d139e3ebd911ed82e8c514cb1b8678448
Karol Szymański <karol2134@op.pl>
2022-05-23 14:12:10 +0200

    Generating a description of notes in notes_main

--- src/Components/Atoms/Notes/notes_data.js
+++ src/Components/Atoms/Notes/notes_data.js
@@ -1,7 +1,19 @@
+import React from 'react';
 import styled from 'styled-components'
 
 const StyledP_2 = styled.p`
   color: rgb(0, 205, 209); margin: 0px 7.5px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  
 `;
 
-export default StyledP_2;
+const NotesData = ({content}) => 
+(
+  <StyledP_2>{content}</StyledP_2>
+);
+
+
+
+export default NotesData;
--- src/Components/Organism/Notes/NotesMain/notes_main.js
+++ src/Components/Organism/Notes/NotesMain/notes_main.js
@@ -1,30 +1,64 @@
-import styled from 'styled-components'
+import styled,{ keyframes } from 'styled-components'
 import React from 'react'
 import NotesElementOne from '../../../Atoms/Notes/notes_rectangle_1'
 import NotesElementTwo from '../../../Atoms/Notes/notes_rectangle_2'
 import NotesData from '../../../Atoms/Notes/notes_data'
 import NotesContainer from '../../../Molecules/Notes/notes_container' 
+import { connect } from "react-redux";
 
+const opacity = keyframes`
+  0%{
+    opacity: 0;
+    display: none;
+  }
+  50%{
+    opacity: 0.5;
+    
+  }
+  100%{
+    
+    opacity: 1;
+    
+  }
+`;
 const StyledDiv = styled.div`
   width: 240px; border-width: 2px; 
   border-style: solid; border-color: rgba(0, 206, 209, 0) rgba(0, 206, 209, 0) rgba(0, 206, 209, 0) rgb(0, 205, 209);
    border-image: initial;
    height: 220px ;
     position: absolute; right: 15%; top: 60%; color: rgb(0, 205, 209);
+    animation: ${opacity} 5s linear;
 `;
 
-const NotesMenu = () =>
+const NotesMenu = ({notes}) =>
 (
  <StyledDiv>
      <NotesElementOne></NotesElementOne>
      <NotesElementTwo></NotesElementTwo>
      <NotesContainer>
-        <NotesData></NotesData>
-        <NotesData></NotesData>
-        <NotesData></NotesData>
+     {notes.map(({content, id}) =>(
+        <NotesData
+        content={content}
+        key = {id}
+        
+        />
+     ))}
      </NotesContainer>
   </StyledDiv> 
 )
 
-export default NotesMenu;
+
+
+
+
+
+const mapStateToProps = state => {
+    const {notes} = state;
+    return {notes};
+}
+
+;
+export default connect(mapStateToProps)(NotesMenu);
+
+
 
