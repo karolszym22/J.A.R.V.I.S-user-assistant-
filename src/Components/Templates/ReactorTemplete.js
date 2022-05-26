b62fc96f6fb414ed01fe6745f49ddee9db0f941a
Karol Szymański <karol2134@op.pl>
2022-05-26 21:36:05 +0200

    Adding TimeMenu in ReactorTemplete.js

--- src/Components/Templates/ReactorTemplete.js
+++ src/Components/Templates/ReactorTemplete.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components';
 import NotesMenu from '../Organism/Notes/NotesMain/notes_main'
 import WeatherMenu from '../Organism/Weather/weather_main'
+import TimeMenu from '../Organism/Time/time-main'
+
 const StyledDiv = styled.div`
   width: 100%;  display: flex; flex-direction: column; 
   align-items: center; color: rgb(80, 208, 228);`;
@@ -11,6 +13,7 @@ const StyledDiv = styled.div`
 const ReactorTemplete = () =>
 (
     <StyledDiv>
+        <TimeMenu></TimeMenu>
         <WeatherMenu ></WeatherMenu>
         <NotesMenu></NotesMenu>
     </StyledDiv>
