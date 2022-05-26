9d48f6e13652e340e6fdc9eeb3228fb7fc2d4e1b
Karol Szymański <karol2134@op.pl>
2022-05-26 21:35:41 +0200

    Adding time information

--- src/Components/Organism/Time/time-main.js
+++ src/Components/Organism/Time/time-main.js
@@ -1,9 +1,61 @@
-import styled from 'styled-components'
-
+import styled,{ keyframes } from 'styled-components'
+import React from 'react'
+import TimeContainer from '../../Molecules/Time/time_container'
+import TimeCircle from '../../Atoms/Time/time_circle'
+import TimeData from '../../Atoms/Time/time_data'
+const opacity = keyframes`
+  0%{
+    opacity: 0;
+    display: none;
+  }
+  50%{
+    opacity: 0.5;
+    
+  }
+  100%{
+    
+    opacity: 1;
+    
+  }
+`;
 const StyledDiv = styled.div`
-  width: 160px; position: absolute; top: 15%; right: 25%; height: 160px; 
+  width: 160px; position: absolute; top: 15%; left: 15%; height: 160px; 
   border: 10px solid rgb(0, 205, 209); border-radius: 100%; justify-content: center; 
-  align-items: center; display: flex; box-shadow: rgba(65, 167, 235, 0.2) 0px 0px 50px 15px, rgba(2, 255, 255, 0.2) 0px 0px 70px 15px inset;
+  align-items: center; display: flex; 
+  animation: ${opacity} 2s ease-out;
+  animation-fill-mode: forwards;
+  opacity: 0 ;
+  animation-delay: 1s;
+  box-shadow: rgba(65, 167, 235, 0.2) 0px 0px 50px 15px, rgba(2, 255, 255, 0.2) 0px 0px 70px 15px inset;
 `;
 
-export default StyledDiv;
+
+const TimeMenu = () =>
+{
+  
+    const [date, setDate] = React.useState(new Date());
+  
+   React.useEffect(() => {
+    var timerID = setInterval( () => Timer(), 1000 );
+    return () => {
+        clearInterval(timerID);
+      };
+    });
+  
+     const Timer = () => {
+      setDate(new Date());
+     }
+
+   return(
+        <StyledDiv>
+          <TimeContainer>
+            <TimeCircle>
+                 <TimeData>{date.getHours()}:{(date.getMinutes() < 10) ? '0' : ''}{date.getMinutes()}</TimeData>
+            </TimeCircle>
+         </TimeContainer>
+        </StyledDiv>  
+        )
+  
+}
+
+export default TimeMenu;
