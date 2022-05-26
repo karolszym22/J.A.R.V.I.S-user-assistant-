c86453b2e2b41f7d9ef3f1e3fb60f89b8f384ce6
Karol Szymański <karol2134@op.pl>
2022-05-26 21:33:16 +0200

    Adding note_add_reactions.js

--- src/Reactions/Note/note_add_reactions.js
+++ src/Reactions/Note/note_add_reactions.js
@@ -1,19 +1,6 @@
-const noteAddReactions = {
-    reactions: [
-    {
-        content: "Pomyślnie dodano notatkę"
-    },
-    {
-        content: "Notatka została dodana"
-    },
-    {
-        content: "Dodano notatkę",
-        
-    },
-    {
-        content: "Kontener został zaktualizowany o nową notatkę",
-        
-    }
-    ]
+const noteAddReactions =  ["Pomyślnie dodano notatkę","Notatka została dodana","Dodano notatkę",
+"Kontener został zaktualizowany o nową notatkę",]
     
-}
+
+
+export default noteAddReactions
