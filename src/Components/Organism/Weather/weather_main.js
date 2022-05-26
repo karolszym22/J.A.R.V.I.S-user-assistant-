8d3ce4786b33eb7ae8ffc01b9d3aefce37d9388c
Karol Szymański <karol2134@op.pl>
2022-05-26 21:35:13 +0200

    Adding weather information in my location

--- src/Components/Organism/Weather/weather_main.js
+++ src/Components/Organism/Weather/weather_main.js
@@ -1,43 +1,94 @@
-import styled from 'styled-components'
+import styled,{ keyframes } from 'styled-components'
 import React from 'react'
 import WeathereLocation from '../../Atoms/Weather/weather_location'
 import WeatherElementOne from '../../Atoms/Weather/weather_rectangle_1'
 import WeatherElementTwo from '../../Atoms/Weather/weather_rectangle_2'
 import WeatherContainer from '../../Molecules/Weather/weather_container'
 import WeatherTemp from '../../Atoms/Weather/weather_temp'
+import axios from 'axios'
+
+const opacity = keyframes`
+  0%{
+    opacity: 0;
+    display: none;
+  }
+  50%{
+    opacity: 0.5;
+    
+  }
+  100%{
+    
+    opacity: 1;
+    
+  }
+`;
 
 const StyledDiv = styled.div`
   width: 240px; height: 400px; position: absolute; top: 10%; right: 7.5%; 
   display: flex; flex-direction: column;
+  animation: ${opacity} 2s ease-out;
+  animation-fill-mode: forwards;
+  opacity: 0 ;
+  animation-delay: 2s;
 `;
 
 
 
 const WeatherContainerInformations= () =>
-(
+{
+  const [latitude, setLatitude] = React.useState('');
+  const [longitude, setLongitude] = React.useState('');
+  const [responseData, setResponseData] = React.useState({});
+  const API_key = '66155567e2e08d7f38875d80ccd3e833'
+ 
+ 
+React.useEffect(() => {
+  navigator.geolocation.getCurrentPosition((position) => {
+    setLatitude(position.coords.latitude)
+    setLongitude(position.coords.longitude)
+  })
   
-  <StyledDiv>
-    <WeathereLocation>Turka</WeathereLocation>
-    < WeatherElementOne>
-            <WeatherElementTwo >
-            <WeatherContainer >
-                     <WeatherTemp>Pogoda</WeatherTemp>
-                     <WeatherTemp>Slonecznie</WeatherTemp>
-                </WeatherContainer>
-                <WeatherContainer >
-                     <WeatherTemp>Temperatura</WeatherTemp>
-                     <WeatherTemp>20*</WeatherTemp>
-                </WeatherContainer>
-                <WeatherContainer >
-                     <WeatherTemp>Wiatr</WeatherTemp>
-                     <WeatherTemp>30/h*</WeatherTemp>
-                </WeatherContainer>
-            </WeatherElementTwo>
-    </WeatherElementOne>
+  if(latitude && longitude > 0)
+   axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_key}`)
+    .then(response => {
+        setResponseData(response.data)
+    })
+  
+  
+}, [latitude, longitude])
 
-  </StyledDiv>
+ const GreetingLocation = (...args) =>
+ {
+      const {name, temp} = args;
+ }
+
+  return (
+    
+  <StyledDiv >
+  <WeathereLocation>{responseData.name}</WeathereLocation>
+  < WeatherElementOne>
+          <WeatherElementTwo>
+          <WeatherContainer >
+                   <WeatherTemp>Pogoda</WeatherTemp>
+                   <WeatherTemp></WeatherTemp>
+              </WeatherContainer>
+              <WeatherContainer >
+                   <WeatherTemp>Temperatura</WeatherTemp>
+                   <WeatherTemp>{responseData.main ? (Math.round(responseData.main.temp)) : ('Loading...')}</WeatherTemp>
+              </WeatherContainer>
+              <WeatherContainer >
+                   <WeatherTemp>Wiatr</WeatherTemp>
+                   <WeatherTemp>30/h*</WeatherTemp>
+              </WeatherContainer>
+          </WeatherElementTwo>
+  </WeatherElementOne>
+
+</StyledDiv>
+  )
+}
   
-)
+  
+
 
 export default WeatherContainerInformations
 
