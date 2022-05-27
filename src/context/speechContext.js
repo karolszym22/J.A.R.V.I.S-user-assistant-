49983abee546c350c12a87e3f0e207973a0d050a
Karol Szymański <karol2134@op.pl>
2022-05-27 18:57:37 +0200

    Creating speechContext.js

---
+++ src/context/speechContext.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+
+
+
+export const SpeechContext = React.createContext();
+
+const SpeechProvider = ({ children }) => {
+    const [speech, setSpeech] = React.useState('');    
+      const { transcript } = useSpeechRecognition();
+      useEffect(()=>{
+        SpeechRecognition.startListening({continuous:false})
+        setSpeech(transcript);
+      })
+    
+    
+
+  return (
+    <SpeechContext.Provider value={speech}>{children}</SpeechContext.Provider>
+  );
+};
+
+export default SpeechProvider
