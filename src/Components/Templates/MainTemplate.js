75c401dc5e5e5b58d309026640cb5ac82bdd47cd
Karol Szymański <karol2134@op.pl>
2022-05-27 15:14:33 +0200

    Adding example speech function

--- src/Components/Templates/MainTemplate.js
+++ src/Components/Templates/MainTemplate.js
@@ -4,18 +4,35 @@ import { ThemeProvider } from "styled-components"
 import GlobalStyle from "../../Theme/GlobalStyles"
 import { theme } from "../../Theme/main_theme"
 import Main from '../Organism/Menu/main-menu'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
 
 
-const MainTemplate = ({children}) => (
+const MainTemplate = ({children}) => {
+
+  const [speech, setSpeech] = React.useState('');    
+  const SpeechtoText = () => {
+    const { transcript,resetTranscript,  } = useSpeechRecognition();
+    useEffect(()=>{
+      SpeechRecognition.startListening({continuous:false})
+      setSpeech(transcript);
+    })
+    console.log(speech)
+  
+  }
+
+  return (
     <div>
-      <GlobalStyle/>
-      <Main></Main>
-      <ThemeProvider theme={theme}>
-        <>{children}</>
-      </ThemeProvider>
-    </div>
-)
+    {SpeechtoText()}
+    <GlobalStyle/>
+    <Main></Main>
+    <ThemeProvider theme={theme}>
+      <>{children}</>
+    </ThemeProvider>
+  </div>
+  )
+   
+}
 
 MainTemplate.propTypes = {
     children: PropTypes.element.isRequired
