7ae06508edba6b5b49e61cec06de7dd8dc830933
Karol Szymański <karol2134@op.pl>
2022-05-27 18:57:22 +0200

    Edit nav_commands.js

--- src/Commands/nav_comands.js
+++ src/Commands/nav_comands.js
@@ -1,1 +1,18 @@
-const navComands = ['Notatki', 'Interfejs', 'Pomoc', 'Szukaj']
+const navComands = [
+    {
+      comands: ["Powrót", "Przełącz na interfejs", "Wróć", "Interfejs"],  
+      link: "/Interfejs"
+    },
+    {
+      comands: ["Dodaj notatkę", "Notatki", "Chciałbym dodać notatkę"],
+      link: "/Notes"
+    },
+    {
+      comands: ["Szukaj", "Poszukaj informacji", "Chcę czegoś poszukać"],  
+      link: "/Szukaj"
+    },
+    {
+      comands: ["Pomoc", "Pomóż mi", "Potrzebuję pomocy"],
+      link: "/Pomoc"
+    }
+]
