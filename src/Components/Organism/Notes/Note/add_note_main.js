2ed608adea928e23ad96750ab499ed97e4ecfcaf
Karol Szymański <karol2134@op.pl>
2022-05-31 19:07:51 +0200

    Adding useEffect in add_note_main.js to hook speech values

--- src/Components/Organism/Notes/Note/add_note_main.js
+++ src/Components/Organism/Notes/Note/add_note_main.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import React from 'react';
+import React, { useEffect } from 'react';
 import Buttom  from '../../../Atoms/Notes/Note/add-note_button';
 import InputContent from '../../../Atoms/Notes/Note/add-note_input_2';
 import InputTittle from '../../../Atoms/Notes/Note/add-note_input';
@@ -7,6 +7,8 @@ import AddNoteContainer from '../../../Molecules/Notes/Note/add-note_container'
 import { connect } from 'react-redux';
 import { addNote as addNoteAction } from '../../../../actions';
 import {Formik, Form, Field} from 'formik'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import {titleCommands, contentCommands} from '../../../../Commands/note_commands';
 
 const StyledDiv = styled.div`
     width: 90%;
@@ -23,12 +25,28 @@ flex-direction: column;
 `
 
 const AddNotes = ({addNote}) =>
-(    
-    <StyledDiv>
+{   
+    
+    const [voiceTitle, setTitle] = React.useState('sdsds')
+    const [voiceContent, setContent] = React.useState('')
+    const { transcript } = useSpeechRecognition();
+
+    useEffect(()=>{
+        SpeechRecognition.startListening({continuous:false})
+        
+                setTitle(transcript)
+                console.log(voiceTitle)
+        
+        
+
+
+    })
+    return(
+        <StyledDiv>
         <AddNoteContainer>
-            <Formik initialValues={{title: '', content: ''}} 
+            <Formik initialValues={{title: voiceTitle, content: voiceContent}} 
             onSubmit={(values) =>{
-           
+                console.log(voiceTitle)
                 addNote(values)
             }}>
 
@@ -44,7 +62,6 @@ const AddNotes = ({addNote}) =>
                    onBlur = {{handleBlur}}
                    value = {values.title}
                    >
-                   
                    </InputTittle>  
                   <InputContent
                     as={Field}
@@ -64,8 +81,10 @@ const AddNotes = ({addNote}) =>
        </AddNoteContainer>
 
     </StyledDiv>
+    )
     
-)
+    
+}
 
 
 
