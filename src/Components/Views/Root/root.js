e8bd7ff7e93ae07d9744222df8ace87298138f36
Karol Szymański <karol2134@op.pl>
2022-05-31 19:02:41 +0200

    Deleting SpeechProvider in root.js

--- src/Components/Views/Root/root.js
+++ src/Components/Views/Root/root.js
@@ -1,6 +1,6 @@
 import React  from 'react'
 import MainTemplate from '../../Templates/MainTemplate';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Help from '../Help/help'
 import Notes from '../Notes/note'
 import Weather from '../Weather/weather'
@@ -8,27 +8,30 @@ import Reactor from '../Reactor/reactor'
 import { Provider } from 'react-redux';
 import store from '../../../store/index'
 import Search from '../Search/Search'
-import SpeechProvider from '../../../context/speechContext';
+import SpeechProvider ,{ SpeechContext } from '../../../context/speechContext';
 const Root = () =>
-( 
+{
 
-<Provider store={store}>
-  <BrowserRouter>
-  <SpeechProvider>
-   <MainTemplate>
-  
-     <Routes>
-           <Route exact path='/' element= {<Reactor />}/>
-           <Route  path='/Notes' element = {<Notes/>}/>
-           <Route  path='/Weather' element = {<Weather/>}/>
-           <Route  path='/Help' element = {<Help/>}/>
-           <Route  path='/Szukaj' element = {<Search/>}/>
-     </Routes>
-    
-   </MainTemplate>
-  </SpeechProvider>
-  </BrowserRouter>
-</Provider>
-)
+
+
+  return(<Provider store={store}>
+    <BrowserRouter>
+     <MainTemplate>
+   <Routes>
+             <Route exact path='/' element= {<Reactor />}/>
+             <Route  path='/notes' element = {<Notes/>}/>
+             <Route  path='/weather' element = {<Weather/>}/>
+             <Route  path='/help' element = {<Help/>}/>
+             <Route  path='/szukaj' element = {<Search/>}/>
+       </Routes>
+      </MainTemplate>
+     </BrowserRouter>
+ 
+  </Provider>)
+
+}
+
+
+
 
 export default Root;
