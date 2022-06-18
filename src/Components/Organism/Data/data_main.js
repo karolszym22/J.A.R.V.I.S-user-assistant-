e5612440fbac825390b27fa837a699c1db9dfc90
Karol Szymański <karol2134@op.pl>
2022-06-18 14:03:20 +0200

    Refactoring data_main.js

--- src/Components/Organism/Data/data_main.js
+++ src/Components/Organism/Data/data_main.js
@@ -1,7 +1,19 @@
+import React from 'react';
 import styled from 'styled-components'
 
 const StyledDiv = styled.div`
-  width: 220px; position: absolute; top: 10%; left: 15%; height: 220px; border: 10px solid rgb(0, 205, 209); border-radius: 100%; justify-content: center; align-items: center; display: flex; box-shadow: rgba(65, 167, 235, 0.2) 0px 0px 50px 15px, rgba(2, 255, 255, 0.2) 0px 0px 70px 15px inset;
+  width: 220px; position: absolute;
+  top: 50%; left: 15%;
+  height: 220px; 
+  border: 10px solid rgb(0, 205, 209);
+  border-radius: 100%;
+  justify-content: center;
+  align-items: center; 
+  display: flex; 
+  box-shadow: rgba(65, 167, 235, 0.2) 0px 0px 50px 15px, rgba(2, 255, 255, 0.2) 0px 0px 70px 15px inset;
+
 `;
+
+
 
 export default StyledDiv
