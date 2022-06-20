c93a4acc12115ac64e331cf4d8c2864e3c410612
Karol Szymański <karol2134@op.pl>
2022-06-20 16:55:19 +0200

    Editing useEffetct() in MainTempate to check if the new reducer works

--- src/Components/Templates/MainTemplate.js
+++ src/Components/Templates/MainTemplate.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
+import { updateBehavior as updateBehaviorAction } from '../../actions/speech_behavior_actions';
 import { ThemeProvider } from "styled-components"
 import GlobalStyle from "../../Theme/GlobalStyles"
 import { theme } from "../../Theme/main_theme"
 import Main from '../Organism/Menu/main_menu'
 import { SpeechContext } from '../../context/SpeechContext'; 
-import { noteCommands } from '../../Commands/note_commands';
+import { includeNote } from '../../Functions/IncludeCommends/include_note_commands';
+import noteInformation from '../../Functions/JarvisRemarks/note_information_remarks';
 
-
-const MainTemplate = ({children}) => {
+const MainTemplate = ({children, updateBehavior}) => {
 
   const speech = useContext(SpeechContext) 
-  useEffect(() =>
-  {
-    if(noteCommands.includes(speech))
-    {
-      console.log("zawiera")
-    }
-  })
+
+      useEffect(() =>
+      {
+         if(includeNote(speech))
+         {
+            updateBehavior(speech)
+            noteInformation();
+         }
+       })
  
   return (
     <div>
@@ -36,4 +39,11 @@ MainTemplate.propTypes = {
     children: PropTypes.element.isRequired
 }
 
-export default MainTemplate;
+
+const mapDispatchToProps = dispatch => ({
+ updateBehavior: (content) => dispatch(updateBehaviorAction(content))
+  
+})
+
+
+export default connect(null, mapDispatchToProps)(MainTemplate);
