5809d66bdf046808af3f259edc67539fc9ee9bce
Karol Szymański <karol2134@op.pl>
2022-06-22 16:56:27 +0200

    Adding commands_interactions function

--- src/Functions/CommandInteractions/commands_interactions.js
+++ src/Functions/CommandInteractions/commands_interactions.js
@@ -1,4 +1,9 @@
-const commands_interactions = () =>
-{
-    
+import React, {useState} from "react";
+import addNoteInteractions from "./Interactions/addNoteInteractions"
+
+const commands_interactions = (speech) => 
+{ 
+    return addNoteInteractions(speech); 
 }
+
+export default commands_interactions
