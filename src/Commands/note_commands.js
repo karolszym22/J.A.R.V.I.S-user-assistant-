eaf48b2ed6807ad7269ef10115dbe7c94465c99c
Karol Szymański <karol2134@op.pl>
2022-06-23 13:04:03 +0200

    Editing note_commands.js: - adding deleteNoteCommands

--- src/Commands/note_commands.js
+++ src/Commands/note_commands.js
@@ -1,4 +1,4 @@
-const titleCommands = ['dodaj tytuł', 'wstaw tytuł', 'tytuł']
-const contentCommands = ['dodaj opis', 'dodaj treść', 'opis']
-const noteCommands = ["Dodaj notatkę", "Chciałbym dodać notatkę", "Dodasz notatke", "Nowa notatka"]
-export {titleCommands, contentCommands, noteCommands} 
+export const titleCommands = ['dodaj tytuł', 'wstaw tytuł', 'tytuł']
+export const contentCommands = ['dodaj opis', 'dodaj treść', 'opis']
+export const addNoteCommands = ["Dodaj notatkę", "Chciałbym dodać notatkę", "Dodasz notatke", "Nowa notatka"]
+export const deleteNoteCommands = ["Usuń notatkę", "Chciałbym usunąć notatkę", "usuniesz notatkę", "usuń mi notatkę"]
