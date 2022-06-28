9ec4cbcb6c9c67cabf53d6ffc62d124a58ecffa4
Karol Szymański <karol2134@op.pl>
2022-06-28 15:19:49 +0200

    Refactoring MainTemplate.js: - import behaviorSubscribe - import VoiceTemplate - change React.useState to useState - adding setDefault function to change mainState to default after 8 seconds - using react redux to get behavior from mapStateToProps function

--- src/Components/Templates/MainTemplate.js
+++ src/Components/Templates/MainTemplate.js
@@ -1,8 +1,10 @@
 import React, {useState,useEffect, useContext } from 'react';
 import PropTypes from 'prop-types'
 import { updateBehavior as updateBehaviorAction } from '../../actions/speech_behavior_actions';
+import behaviorSubscribe from '../../Functions/VoiceSubscribe/voice_interactions';
 import { ThemeProvider } from "styled-components"
 import { connect } from 'react-redux/es/exports';
+import VoiceTemplate from './VoiceTemplate';
 import GlobalStyle from "../../Theme/GlobalStyles"
 import { theme } from "../../Theme/main_theme"
 import Main from '../Organism/Menu/main_menu'
@@ -10,28 +12,41 @@ import commands_interactions from '../../Functions/CommandInteractions/commands_
 import { SpeechContext } from "../../context/SpeechContext";
 
 const MainTemplate = ({children, updateBehavior}) => {
-  
-  const [state, setMainState] = React.useState('')
-  const speech = useContext(SpeechContext) 
 
+  const [state, setMainState] = useState('')
+  const speech = useContext(SpeechContext) 
   console.log(speech)
+
       const setBehavior = () => {
         setMainState(commands_interactions(speech))
+        
+      }
+      
+       useEffect(() =>
+      {
         if(state?.length > 0)
         {
           updateBehavior(state)
-        }
-      }
+          behaviorSubscribe(state)
+        } 
+      }, [state])
+
 
       useEffect(() =>
       {
         setBehavior()
+        const setDefault = setTimeout(() => {
+            updateBehavior('xxxxxxx')
+        }, 8000);
+           return () => clearTimeout(setDefault);
       })
+      
  
   return (
     <div>
     <GlobalStyle/>
        <Main></Main>
+       <VoiceTemplate state = {state}></VoiceTemplate>
        <ThemeProvider theme={theme}>
       <>{children}</>
     </ThemeProvider>
@@ -48,8 +63,13 @@ const mapDispatchToProps = dispatch => ({
   updateBehavior: (content) => dispatch(updateBehaviorAction(content))
    
  })
+
+ const mapStateToProps = state => {
+  const {behavior} = state.behavior;
+  return {behavior};
+}
  
  
- export default connect(null, mapDispatchToProps)(MainTemplate);
+ export default connect(null, mapDispatchToProps )(MainTemplate);
 
 
