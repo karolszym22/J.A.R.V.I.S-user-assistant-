ca8827665fe9729a22744804e26b72bae14deb8a
Karol Szymański <karol2134@op.pl>
2022-07-02 20:21:01 +0200

    Adding new case in voice_interactions.js: - 'deleteNote'

--- src/Functions/VoiceSubscribe/voice_interactions.js
+++ src/Functions/VoiceSubscribe/voice_interactions.js
@@ -2,7 +2,10 @@ const behaviorSubscribe = (behavior) =>{
     switch (behavior) {
         case 'addNote':
           window.responsiveVoice.speak(("Podaj tytuł oraz opis"),"Polish Male")
-            break;
+        break;
+        case 'deleteNote':
+          window.responsiveVoice.speak(("Podaj tytuł notatki którą chcesz usunąc"),"Polish Male")
+        break
           default: 
           
         
