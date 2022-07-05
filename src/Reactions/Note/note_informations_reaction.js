aaadb4e9ba27f092123fb100167c99fb4e84b819
Karol Szymański <karol2134@op.pl>
2022-07-05 14:40:12 +0200

    Renaming reaction files

--- src/Reactions/Note/note_informations_reaction.js
+++
@@ -1,3 +0,0 @@
-const noteInformationsReactions =  ["Musisz podać mi tytuł oraz opis","Podaj tytuł oraz opis","Tytuł i opis?"]
-    
-export default noteInformationsReactions
---
+++ src/Reactions/Note/remove_informations_reaction.js
@@ -0,0 +1,8 @@
+const noteInformationsReactions = 
+ [
+   "Musisz podać mi tytuł notatki którą chcesz usunąc",
+   "Podaj tytuł notatki",
+   "Aby usunąc notatkę musisz podać tytuł"
+]
+    
+export default noteInformationsReactions
