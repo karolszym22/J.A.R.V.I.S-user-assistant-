5cbcbe49fe7c4dbec24666d8c32766cd866b1b4c
Karol Szymański <karol2134@op.pl>
2022-07-06 21:42:10 +0200

    Adding new states in VoiceTemplate.js: -title -description -noteValues -noteTitle

--- src/Components/Templates/VoiceTemplate.js
+++ src/Components/Templates/VoiceTemplate.js
@@ -1,23 +1,35 @@
 import React, {useEffect, useContext, useState} from 'react';
 import { SpeechContext } from "../../context/SpeechContext";
 import { connect } from 'react-redux/es/exports';
-import addNoteInterction from '../../Functions/CommandInteractions/Interactions/add_note_interaction';
+import getNoteValues from '../../Functions/CommandInteractions/Interactions/set_note_values';
+import { addNote as addNoteAction } from '../../actions/handle_note_actions'
+import addNoteValidation from '../../Functions/CommandInteractions/Interactions/Validations/Note/add_note_validation';
 
 const VoiceTemplate = ({behavior}) => {
 
     const speech = useContext(SpeechContext) 
     const [currentSpeech, setCurrentSpeech] = useState('')
     const [currentState, setCurrentState] = useState('')
-  
+
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+
+    const [noteValues, setNoteValues] = useState(false)
+    const [noteTitle, setNoteTitle] = useState(false)
+    
+
     useEffect(() =>
     {
         setCurrentSpeech(speech)
         setCurrentState(behavior)
-        
+
         switch(currentState) {
             case 'addNote':
-                addNoteInterction(currentSpeech)
-              break;
+              { 
+                getNoteValues(currentSpeech, setTitle, setDescription)
+                addNoteValidation(title,description, setNoteValues)
+              }
+            break;
             case 'deleteNote':
               // code block
               break;
@@ -37,5 +49,10 @@ const mapStateToProps = state => {
     return {behavior};
   }
 
+  const mapDispatchToProps = dispatch => ({
+    
+    addNote: (noteContent) => dispatch(addNoteAction(noteContent))
+    
+})
 
-  export default connect(mapStateToProps)(VoiceTemplate);
+  export default connect(mapStateToProps,mapDispatchToProps)(VoiceTemplate);
