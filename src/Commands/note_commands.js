e37819324611b781d7057d5748d815673441404c
Karol Szymański <karol2134@op.pl>
2022-07-06 16:01:21 +0200

    Refactoring note_commands.js - commands start with a capital letter

--- src/Commands/note_commands.js
+++ src/Commands/note_commands.js
@@ -1,4 +1,4 @@
-export const titleCommands = ['dodaj tytuł', 'wstaw tytuł', 'tytuł']
-export const contentCommands = ['dodaj opis', 'dodaj treść', 'opis']
+export const titleCommands = ['Dodaj tytuł', 'wstaw tytuł', 'tytuł']
+export const contentCommands = ['Dodaj opis', 'Dodaj treść', 'Opis']
 export const addNoteCommands = ["Dodaj notatkę", "Chciałbym dodać notatkę", "Dodasz notatke", "Nowa notatka"]
 export const deleteNoteCommands = ["Usuń notatkę", "Chciałbym usunąć notatkę", "usuniesz notatkę", "usuń mi notatkę"]
