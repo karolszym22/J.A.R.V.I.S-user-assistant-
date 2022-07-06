b6db88e910b52d64fa823ba0673520237cc0de86
Karol Szymański <karol2134@op.pl>
2022-07-06 16:02:15 +0200

    Editing add_note_interactions.js: - adding a function that breaks down the spoken sentence into command and value

--- src/Functions/CommandInteractions/Interactions/add_note_interaction.js
+++ src/Functions/CommandInteractions/Interactions/add_note_interaction.js
@@ -1,12 +1,38 @@
 
-import {useEffect, useState} from "react"
+import { titleCommands, contentCommands } from "../../../Commands/note_commands";
 
+let title = '' 
+let description = ''
 
-const addNoteInterction = (values) =>
+const addNoteInterction = (...args) =>
 {
- let title = '' 
- let description = ''
+ 
+  const [values, setTitle, setDescription] = args
+
+ 
+  titleCommands.forEach((e) =>
+  {
+      const currentCommand = values.substr(0, e.length)
+      if(currentCommand === e)
+      {
+        const currentTitle = values.substr(e.length, values.length)
+        setTitle(currentTitle);
+      }
+  })
+
+
+  contentCommands.forEach((e) =>{
+    const currentCommand = values.substr(0, e.length)
+     if(currentCommand === e)
+     {
+       const currentTitle = values.substr(e.length, values.length)
+       setDescription(currentTitle)
+     }
+  })
+
+
 
 }
+
 
 export default addNoteInterction;
