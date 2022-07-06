d34e58ba0fb3a069efacd4addaae0f7f1a7c0880
Karol Szymański <karol2134@op.pl>
2022-07-06 17:42:06 +0200

    Editing add_note_validations: - checks if the length of the title and description is greater than zero

--- src/Functions/CommandInteractions/Interactions/Validations/Note/add_note_validation.js
+++ src/Functions/CommandInteractions/Interactions/Validations/Note/add_note_validation.js
@@ -1,5 +1,15 @@
 
-const add_note_validation = () =>
+const add_note_validation = (...args) =>
 {
-  
+  let is_title = false;
+  let is_description = false  
+  const VALUE_lENGTH = 0;  
+
+  const {title,description, setNoteValues} = args
+
+      title?.length > VALUE_lENGTH ? is_title = true : is_title = false
+      description?.length > VALUE_lENGTH ? is_description = true : is_description = false
+        is_title === true && is_description ===true ? setNoteValues(true) : setNoteValues(false)
 }
+
+export default add_note_validation()
