f746823115783345b83ba37da672a927278234ad
Karol Szymański <karol2134@op.pl>
2022-07-08 18:16:15 +0200

    Refactoring add_note_validation.js: - get arguments from array

--- src/Functions/CommandInteractions/Interactions/Validations/Note/add_note_validation.js
+++ src/Functions/CommandInteractions/Interactions/Validations/Note/add_note_validation.js
@@ -5,11 +5,12 @@ const addNoteValidation = (...args) =>
   let is_description = false  
   const EMPTY_VALUE = 0;  
 
-  const {title,description, setNoteValues} = args
+  const [title,description, setNoteValues] = args
 
       title?.length >EMPTY_VALUE ? is_title = true : is_title = false
       description?.length >EMPTY_VALUE ? is_description = true : is_description = false
         is_title && is_description ===true ? setNoteValues(true) : setNoteValues(false)
+        
 }
 
-export default addNoteValidation()
+export default addNoteValidation
