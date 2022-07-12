8b7a982e5bf8e5cb1da8f40adce418b38c80308c
Karol Szymański <karol2134@op.pl>
2022-07-12 21:42:20 +0200

    renaming a file set_note_values.js for get_note_values.js

---
+++ src/Functions/CommandInteractions/Interactions/get_note_values.js
@@ -0,0 +1,15 @@
+import cutTitle from "./Helpers/cut_title"
+import cutDescription from "./Helpers/cut_description"
+
+const getNoteValues = (...args) =>
+{
+ 
+  const [values, setTitle, setDescription] = args
+    
+    
+ 
+}
+
+
+
+export default getNoteValues;
