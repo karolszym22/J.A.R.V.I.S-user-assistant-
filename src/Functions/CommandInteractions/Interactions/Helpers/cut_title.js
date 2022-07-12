56d3631452c29896350370b4b050f47f2f59f683
Karol Szymański <karol2134@op.pl>
2022-07-12 21:40:14 +0200

    Refactoring cut_title.js

--- src/Functions/CommandInteractions/Interactions/Helpers/cut_title.js
+++ src/Functions/CommandInteractions/Interactions/Helpers/cut_title.js
@@ -1,20 +1,29 @@
-import { titleCommands } from "../../../../Commands/note_commands"
+import { addTitleCommands, deleteNoteCommands } from "../../../../Commands/note_commands"
 
 
 const cutTitle = (...args) =>
    {
       const [values, setTitle] = args
 
-     titleCommands.forEach((e) =>{
-       const currentCommand = values.substr(0, e.length)
-        if(currentCommand === e)
-         {
-           const currentTitle = values.substr(e.length, values.length)
-           setTitle(currentTitle);
-         }
-     })
+        addTitleCommands.forEach((e) =>{
+          breakValues(e, setTitle, values)
+        })
+
+        deleteNoteCommands.forEach((e) =>{
+              breakValues(e, setTitle, values)
+       })
 
   
 }
 
+const breakValues = (element, setTitle, values) =>
+{
+  const currentCommand = values.substr(0, element.length)
+
+  if(currentCommand === element)
+   {
+      const currentTitle = values.substr(element.length, values.length)
+      setTitle(currentTitle);
+   }
+}
 export default cutTitle
