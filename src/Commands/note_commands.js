ebff632ce66a4fd313f18f89bc27c184cbfc8909
Karol Szymański <karol2134@op.pl>
2022-07-12 21:41:10 +0200

    Adding new commands in note_commands.js

--- src/Commands/note_commands.js
+++ src/Commands/note_commands.js
@@ -1,4 +1,5 @@
-export const titleCommands = ['dodaj tytuł', 'wstaw tytuł', 'tytuł', 'a tytuł to będzie','a tytuł to będzie', 'a tytuł  niech będzie']
-export const contentCommands = ['dodaj opis', 'dodaj treść', 'opis', 'a opis to będzie', 'opis to będzie']
 export const addNoteCommands = ["dodaj notatkę", "chciałbym dodać notatkę", "dodasz notatke", "nowa notatka"]
+export const addTitleCommands = ['dodaj tytuł', 'wstaw tytuł', 'tytuł', 'tytuł to będzie','a tytuł to będzie', 'a tytuł  niech będzie']
+export const addContentCommands = ['dodaj opis', 'dodaj treść', 'a opis to będzie', 'opis to będzie']
 export const deleteNoteCommands = ["usuń notatkę", "chciałbym usunąć notatkę", "usuniesz notatkę", "usuń mi notatkę"]
+export const deleteTitleCommands = ['notatkę o tytule', 'tytuł to']
